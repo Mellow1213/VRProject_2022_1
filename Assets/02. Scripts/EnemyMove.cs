60532983ec72868b847926ba24b25cbc86d7ef19
Mellow1213 <dpcks7282@gmail.com>
2022-05-12 12:25:54 +0900

    Enemy 이동 방식 구현

---
+++ Assets/02. Scripts/EnemyMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public GameObject[] enemyPosition;
+
+    float timeRate;
+    Transform playerPos;
+    int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = GameObject.Find("PlayerPos").transform;
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(playerPos);
+
+        timeRate += Time.deltaTime;
+
+        if (timeRate > 5f)
+        {
+            i = Random.Range(0, enemyPosition.Length);
+            timeRate = 0;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, enemyPosition[i].transform.position, Time.deltaTime);
+    }
+
+    
+
+
+}
