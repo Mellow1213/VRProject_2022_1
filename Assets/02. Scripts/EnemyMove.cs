bf7f73d8d506b9675133b041b1df60ad81c72de7
Mellow1213 <dpcks7282@gmail.com>
2022-05-18 00:27:37 +0900

    DestroyedCity 에셋 추가

--- Assets/02. Scripts/EnemyMove.cs
+++ Assets/02. Scripts/EnemyMove.cs
@@ -6,14 +6,11 @@ public class EnemyMove : MonoBehaviour
 {
     public GameObject[] enemyPosition;
 
-    float timeRate;
     Transform playerPos;
-    int i;
     // Start is called before the first frame update
     void Start()
     {
         playerPos = GameObject.Find("PlayerPos").transform;
-        i = 0;
     }
 
     // Update is called once per frame
@@ -21,13 +18,6 @@ public class EnemyMove : MonoBehaviour
     {
         transform.LookAt(playerPos);
 
-        timeRate += Time.deltaTime;
-
-        if (timeRate > 5f)
-        {
-            i = Random.Range(0, enemyPosition.Length);
-            timeRate = 0;
-        }
     }
 
     
