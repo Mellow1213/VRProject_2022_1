c3c09a2fc46e80371cad3eaa6fca824f93703bba
Mellow1213 <dpcks7282@gmail.com>
2022-05-23 00:11:26 +0900

    미니건 에셋 업데이트
    1. 원래 있던 미니건 오브젝트를 blender로 오브젝트 구분, 오브젝트 따로 둠
    
    2. 공격 시 분리된 총구 오브젝트 회전하는 기능 추가. Player_Attack.cs 스크립트에서 GunRotatet()함수가 관리

--- Assets/02. Scripts/Helicopter/Propeller.cs
+++ Assets/02. Scripts/Helicopter/Propeller.cs
@@ -14,7 +14,6 @@ public class Propeller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("transform.eulerAngles.z = " + transform.eulerAngles.z);
         transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
         if (transform.eulerAngles.z > 360 || transform.eulerAngles.z < 0)
             transform.eulerAngles = new Vector3(-90, 0, 0);
