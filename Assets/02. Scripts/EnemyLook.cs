8fdfadd3993d07673d28ba3433ff9b72d28ac01f
lbd0 <dlqkek8@naver.com>
2022-05-24 00:39:10 +0900

    적 터렛 대충 완료

--- Assets/02. Scripts/EnemyLook.cs
+++ Assets/02. Scripts/EnemyLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyLook : MonoBehaviour
 {
-    public GameObject target;
+    private GameObject target;
 
     // Start is called before the first frame update
     void Start()
