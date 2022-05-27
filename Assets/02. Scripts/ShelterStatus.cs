777f1078a984c57fe28e23ea98fcced45c78eecc
lbd0 <dlqkek8@naver.com>
2022-05-28 03:18:02 +0900

    맵 설치 및 지상적

---
+++ Assets/02. Scripts/ShelterStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelterStatus : MonoBehaviour
+{
+    public GameObject explosion;
+    private Transform shelter;
+    private int HP;
+    public static bool isBoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shelter = this.transform.parent;
+        HP = 50;
+        isBoom = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+            HP -= 10;
+        Debug.Log(HP);
+        if (HP <= 0)
+            Explosion();
+    }
+
+    void Explosion()
+    {
+        this.gameObject.SetActive(false);
+        explosion.SetActive(true);
+        Destroy(shelter.gameObject, 3.0f);
+        isBoom = true;
+    }
+}
