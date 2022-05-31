7d5e34ac5fb5efa014dffb055e0e5ceb48cafcea
Mellow1213 <dpcks7282@gmail.com>
2022-06-01 00:13:49 +0900

    flyEnemy 공격 - 미사일 구현

---
+++ Assets/02. Scripts/Enemy/FlyerEnemy/EnemyFire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+{
+    public GameObject bullet;
+    float timer = 0;
+    public float fireDelay;
+    public GameObject firePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (fireDelay is 0)
+            fireDelay = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > fireDelay)
+        {
+            GameObject temp = Instantiate(bullet, firePos.transform.position, firePos.transform.rotation);
+            temp.transform.parent = firePos.transform;
+            timer = 0f;
+        }
+    }
+}
---
+++ Assets/02. Scripts/Enemy/FlyerEnemy/FlyEnemyBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEnemyBullet : MonoBehaviour
+{
+    GameObject hitPoint;
+    float speed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitPoint = GameObject.Find("HitPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, hitPoint.transform.position, Mathf.Lerp(speed, 6f, 3*Time.deltaTime));
+
+        transform.LookAt(hitPoint.transform);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(hitPoint))
+        {
+            Debug.Log("확인");
+            Destroy(gameObject);
+        }
+    }
+}
