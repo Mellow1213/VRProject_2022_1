fd2a907b0666c9757ce41bc2b6530da9062047b8
Mellow1213 <dpcks7282@gmail.com>
2022-06-01 16:53:40 +0900

    ScoreUI 추가
    헬기 내부 특수능력 버튼쪽에 현재 score 보이는 UI 추가

---
+++ Assets/02. Scripts/Helicopter/ScoreUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+    public TextMeshProUGUI scoreUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreUI.text = "Score\n"+GameManager.Instance.score;
+    }
+}
