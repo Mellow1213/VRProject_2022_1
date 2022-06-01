326a9509061507030d528fcf06572fa04d76aee0
lbd0 <dlqkek8@naver.com>
2022-06-01 11:23:24 +0900

    map3,4 폭발물 설치

---
+++ Assets/02. Scripts/ExplosiveCtrl.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveCtrl : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 20);
+
+        for(int i=0; i<colliders.Length; i++)
+        {
+            Rigidbody enemy = colliders[i].GetComponent<Rigidbody>();
+            enemy.AddExplosionForce
+        }
+    }
+}
