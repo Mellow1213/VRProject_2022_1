367b77c0f63a105cd01c7c8f96385014137cb762
lbd0 <dlqkek8@naver.com>
2022-06-01 21:04:25 +0900

    포탄 사운드 수정

--- Assets/02. Scripts/Enemy/TurretEnemy/EnemyGunCtrl.cs
+++ Assets/02. Scripts/Enemy/TurretEnemy/EnemyGunCtrl.cs
@@ -7,14 +7,15 @@ public class EnemyGunCtrl : MonoBehaviour
     public GameObject bullet;
     public GameObject bulletSpawn;
     public AudioClip fireSound;
-    private bool isFire;
+    private GameObject Turrets;
+    public bool isFire;
 
     // Start is called before the first frame update
     void Start()
     {
+        Turrets = GameObject.Find("EnemyTurrets");
         isFire = false;
-          
-        
+    
     }
 
     // Update is called once per frame
@@ -32,8 +33,8 @@ public class EnemyGunCtrl : MonoBehaviour
     IEnumerator Fire()
     {
         yield return new WaitForSeconds(10.0f);
-        bulletSpawn.GetComponent<AudioSource>().PlayOneShot(fireSound);
         Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        Turrets.GetComponent<AudioSource>().PlayOneShot(fireSound);
 
         isFire = false;    
     }
---
+++ Assets/02. Scripts/Enemy/TurretEnemy/FireSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSound : MonoBehaviour
+{
+    public AudioClip fireSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Firesound();
+    }
+
+    IEnumerator Firesound()
+    {
+        yield return new WaitForSeconds(10.0f);
+        Debug.Log("소리");
+        this.gameObject.GetComponent<AudioSource>().PlayOneShot(fireSound);
+    }
+}
